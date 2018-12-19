f703bb2049e3b22c8d8c4ff10941a3bec3102844
WardiD <dariusz.wardega@gmail.com>
2018-12-19 23:19:10 +0100

    Modyfing titles of tab menu

--- src/app/GUI/GUIController.java
+++ src/app/GUI/GUIController.java
@@ -15,7 +15,7 @@ import java.io.File;
 
 public class GUIController {
 
-    
+
     @FXML
     private Button loadImageButton;
 
---
+++ src/app/GUI/Main.java
@@ -0,0 +1,24 @@
+package app.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        primaryStage.setTitle("Basic Operations on Images ");
+        primaryStage.setScene(new Scene(root, 1600, 900));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
